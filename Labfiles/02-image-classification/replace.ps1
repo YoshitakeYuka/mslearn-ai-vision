b1c2543a5c78ee32ad4146c7ebb44ff8777e048d
Yuka Yoshitake <41535067+YoshitakeYuka@users.noreply.github.com>
2024-05-23 22:51:40 +0900

    Update replace.ps1

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,2 @@
-$storageAcct = '<storageAccount>'
+$storageAcct = 'customclassifyetennuit'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
